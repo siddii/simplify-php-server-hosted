10af443131f55a2731b2a9dc7cdaacb7c733d954
Siddique Hameed <siddique_hameed@mastercard.com>
2015-04-13 09:01:22 -0500

    HTTPS redirect

--- index.php
+++ index.php
@@ -40,16 +40,16 @@
  </head>
 
  <body>
- <h1 class="text-center"><?php echo "Server is up and running successfully!" ?></h1>
+ <h1 class="text-center"><?php echo "Server is up and running!" ?></h1>
 
- <h3 class="text-center">
+ <h2 class="text-center">
 	 For testing Hosted Payments here is a <a href="hosted-payments.php">sample page</a>.
- </h3>
+ </h2>
  <br/>
 
- <h3 class="text-center">
+ <h2 class="text-center">
 	 For running payments from your mobile applications, you can POST <i>simplifyToken</i> & <i>amount</i> to charge.php...
- </h3>
+ </h2>
 
  </body>
 
